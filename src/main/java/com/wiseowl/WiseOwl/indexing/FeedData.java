c5e72d9a466e58c63eab18751b5fa6ac1e1db9e6
SumitBanger <sumitbanger1995@gmail.com>
2017-02-06 20:38:21 +0530

    support for Indexing using GUI added.

--- src/main/java/com/wiseowl/WiseOwl/indexing/FeedData.java
+++ src/main/java/com/wiseowl/WiseOwl/indexing/FeedData.java
@@ -1,98 +1,87 @@
-
 package com.wiseowl.WiseOwl.indexing;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 //import java.nio.file.Files;
 import java.util.Scanner;
-import com.wiseowl.WiseOwl.wikiClean.*;
-import org.apache.solr.client.solrj.*;
-import org.apache.solr.client.solrj.request.AbstractUpdateRequest;
-//import org.apache.solr.client.solrj.SolrRequest;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
-import org.apache.solr.common.SolrInputDocument;
+
+import javax.swing.JOptionPane;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.benchmark.byTask.feeds.DocData;
 import org.apache.lucene.benchmark.byTask.feeds.EnwikiContentSource;
 import org.apache.lucene.benchmark.byTask.feeds.NoMoreDataException;
 import org.apache.lucene.benchmark.byTask.utils.Config;
+import org.apache.solr.client.solrj.SolrClient;
+//import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.request.AbstractUpdateRequest;
+import org.apache.solr.client.solrj.request.CollectionAdminRequest.List;
+import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
+import org.apache.solr.common.SolrInputDocument;
+
+import com.wiseowl.WiseOwl.wikiClean.WikiClean;
+import com.wiseowl.WiseOwl.wikiClean.WikiCleanBuilder;
+
+import indexGUI.stop_pause;
 public class FeedData {
-	//static String indexPath="../toIndex/";
-		//FeedData feedData=null;
-		String indexType=null;
-		String folder;
-		long skip=0;
+		public static String indexType=null;
+		public static String url,filez="",name="";
+		public static int skip=0,back=0;
+		public static RandomAccessFile randomAccessFile = null;
+        public static int i,numDocs,batchSize = 5;
 		private transient static Log log = LogFactory.getLog(FeedData.class);
-		FeedData(String indexType,String folder,int skip)
-		{
-			this.indexType=indexType;
-			this.folder=folder;
-			this.skip=skip;
-		}
-		public static void main (String[] args) throws IOException, SolrServerException {
-				
-			if(args.length<2)
-			{
-				System.out.println("Require two arguments: \n <index type> <folder location> <documents Indexed>");
-				return ;
-			}
-			FeedData feedData=null;
-			if(args.length == 2)	
-				feedData=new FeedData(args[0],args[1],0);
-			else if(args.length==3)
-				feedData=new FeedData(args[0],args[1],Integer.parseInt(args[2]));
-			feedData.index();
-
-		}
-		public int index() throws IOException, SolrServerException {
-		    return index(Integer.MAX_VALUE, 5);
-		  }
-
-		public int index(int numDocs, int batchSize) throws IOException, SolrServerException
-		{
-			SolrClient client = new HttpSolrClient.Builder("http://localhost:8983/solr/WiseOwl").build();
-			File[] files = new File(folder).listFiles();
-			if(files==null)
-			{
-				System.out.println(folder +" Folder Not Found Please Enter a correct folder name!");
-				return 0;
-			}
+        public static boolean running = false,isPaused=false;    
+        public static void reset()
+        {
+        	indexType=null;
+        	filez="";	name="";
+        	skip=0;		back=0;
+        	running = false;	isPaused=false; 
+        }
+		public int index() throws IOException, SolrServerException
+		{       numDocs = Integer.MAX_VALUE;
+                        i=0;
+			SolrClient client = new HttpSolrClient.Builder(url).build();
 			int result=0;
 			long id=this.readID();
-			if(indexType.equalsIgnoreCase("other"))
+			if(indexType.equalsIgnoreCase("others"))
 			{
 			ContentStreamUpdateRequest req = new ContentStreamUpdateRequest("/update/extract");
-				for (File file : files) {
-					   if (file.isFile()) { 	
+			File file = new File(filez);	
+                       if (file.isFile()) { 	
 					   req.setParam("literal.id", ""+this.readID());
-					   req.addFile(new File(folder+file.getName()),"");
+					   req.addFile(file,"");
+					   //new File(file.getName()),""
+					   System.out.println("Before Action "+FeedData.filez);
 					   req.setAction(AbstractUpdateRequest.ACTION.COMMIT, true, true);
+					   System.out.println("After Action "+FeedData.filez);
 					   System.out.println("Sending "+file.getName()+" to WiseOwl to make him Wiser!");
 					   client.request(req);
 					   System.out.println("Wise Owl Learned about "+file.getName()+" and got Wiser!");
 					   id++;
-					  }
 					}
 			}else if(indexType.equalsIgnoreCase("wikipedia"))
 			{
-				for (File file : files) {
-					
-				    if (file.isFile() && file.getName().endsWith(".xml") ) {
-				    	
+            	System.out.println("in index wikipedia ");            	
+				File file = new File(filez);
+            	System.out.println("after list files ");
+						if(file.isFile() && file.getName().endsWith(".xml") ) {
+                    	System.out.println("in index .xml ");
 				    	EnwikiContentSource contentSource = new EnwikiContentSource();
 				    	WikiClean cleaner=new WikiCleanBuilder().build();
 				        Properties properties = new Properties();
+				        //fileName = config.get("docs.file", null);
 				        String filePath = file.getAbsolutePath();
 				        properties.setProperty("docs.file", filePath);
 				        properties.setProperty("doc.maker.forever", "false");
 				        contentSource.setConfig(new Config(properties));
 				        contentSource.resetInputs();
+				        //docMaker.openFile();
 				        DocData docData1 = new DocData();
-				        for(int j=0;j<skip;j++)
+				        for(int j=0;j<FeedData.skip;j++)
 				        {
 				        	try {
 								docData1=contentSource.getNextDocData(docData1);
@@ -102,16 +91,16 @@ public class FeedData {
 							}
 				        	System.out.println(docData1.getTitle());
 				        }
-				        List<SolrInputDocument> docs = new ArrayList<SolrInputDocument>(1000);
-				        int i = 0;
+				        ArrayList<SolrInputDocument> docs = new ArrayList<SolrInputDocument>(1000);
+				        i = FeedData.skip;
 				        SolrInputDocument sDoc = null;
 				        long start = System.currentTimeMillis();
 				        try {
 				          DocData docData = new DocData();
-				          while ((docData = contentSource.getNextDocData(docData)) != null && i < numDocs) 
+
+                          while ((docData = contentSource.getNextDocData(docData)) != null && i < numDocs && isPaused==false && running==true) 
 				          {
 				            int mod = i % batchSize;
-
 				            sDoc = new SolrInputDocument();
 				            docs.add(sDoc);
 				            id++;
@@ -120,17 +109,30 @@ public class FeedData {
 				            sDoc.addField("title", docData.getTitle());
 				            sDoc.addField("author", docData.getName());
 				            sDoc.addField("content_type", "Wikipedia Data");
+                            String str = docData.getTitle();
+                            indexGUI.stop_pause.area.append(str+"\n");
+                            try {
+								Thread.sleep(1000);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 				            //client.add(sDoc);
 				            if (mod == batchSize - 1) {
+				            	System.out.println("Commiting ");
 				              log.info("Sending: " + docs.size() + " docs" + " total sent for this file: " + i);
 				             client.add(docs);
-					         client.commit();
-				             this.writeID(id);
+				             client.commit();
+				             FeedData.skip=i;
+				             System.out.println("now skip is  "+ FeedData.skip);
+				             addData(FeedData.randomAccessFile.getFilePointer()-back-1);
+				             System.out.println("now seek is  "+ (FeedData.randomAccessFile.getFilePointer()-back-1));
+//                           this.writeID(id);
 				              docs.clear();
 				            }
-				            
 				            i++;
 				          }
+                          System.out.println("Value of Ispaused : "+FeedData.isPaused);
 				        } catch (NoMoreDataException e) {
 
 				        }
@@ -139,15 +141,26 @@ public class FeedData {
 				          log.info("Indexing took " + (finish - start) + " ms");
 				        }
 				        if (docs.size() > 0) {
+				         // client.add(docs);
 				        }
 				        result = i + docs.size();
 				        System.out.println("finished"+result);
-				      }
+				      
 					}
+				System.out.println("after for");
 			}
+			if(isPaused==false){
 			System.out.println("finished");
+			FeedData.skip=-1;
+			}
+			else{
+				System.out.println("Indexing is Paused .");
+				System.out.println("value of skip : "+FeedData.skip);
+			}
+			addData(FeedData.randomAccessFile.getFilePointer()-back-1);
 			this.writeID(id);
 			client.commit();
+			FeedData.running=false;
 	        client.optimize();
 			return result;
 		}
@@ -157,12 +170,12 @@ public class FeedData {
 			long id=0;
 			Scanner s=null;
 			try {
-			s=new Scanner(new File(folder+"settings/uniqueId.txt"));
+			s=new Scanner(new File("toIndex/settings/uniqueId.txt"));
 			id=s.nextLong();
 			
 			}catch (FileNotFoundException e)
 			{
-				System.out.println("Can't find uniqueId.txt , It must contain starting point of doc Id as integer in ./toIndex/settings folder");
+				System.out.println("Can't find uniqueId.txt , It must contain starting point of doc Id as integer in ./toIndex/settings filez");
 			}finally
 			{
 				s.close();
@@ -175,16 +188,86 @@ public class FeedData {
 
 			try {
 			    writer = new BufferedWriter(new OutputStreamWriter(
-			          new FileOutputStream(folder+"settings/uniqueId.txt"), "utf-8"));
+			          new FileOutputStream("toIndex/settings/uniqueId.txt"), "utf-8"));
 			    writer.write(""+id);
 			} catch (IOException ex) {
 			  // report
 			} finally {
-			   try {writer.close();}
-			   catch (Exception ex) {
-				   /*ignore*/
-				   }
+			   try {writer.close();} catch (Exception ex) {/*ignore*/}
 			}
 		}
+		public static int readSkip() throws IOException
+	    {//new File("toIndex/settings/indexed.txt"
+				try {
+					randomAccessFile=new RandomAccessFile("toIndex/settings/"+FeedData.indexType+".txt","rw");
+				}catch (IOException e)
+				{
+					System.out.println("Can't find one of the files\nUsage MakeCorpus <sentence templates> <names> <output File>");
+				}
+				String value=null;
+				long size = randomAccessFile.length();
+				if(size==0)
+					addData(size);
+			    //Finding position of File Pointer
+			    long positionOfFilePointer = randomAccessFile.getFilePointer();
+			    value=searchData();
+			    if(value==null)
+			    	return -2;
+			    else
+	                return Integer.parseInt(value);
+		}
+
+	    public static void addData(long size) throws IOException {
+
+	        //seeking position at end of file
+	    	System.out.println("the seek is : "+size);
+	        randomAccessFile.seek(size);
+	        //char off[] = new char[8];
+	        String s = ""+FeedData.skip;
+	        int l=s.length(),i;
+	        /*for(int i=0;i<7;i++)
+	        {
+	        	if(i<(s).length())
+	        		off[i]=s.charAt(i);
+	        	else
+	        	off[i]=' ';
+	        }
+	        System.out.println(off);
+	        String s1 = off.toString();*/
+	        for(i=0;i<7-l;i++)
+	        {
+	        	s+=" ";
+	        }
+	        System.out.println(s);
+	        StringBuffer stringBuffer = new StringBuffer();
+	        	stringBuffer.append(FeedData.name).append(":").append(s);
+	        back = stringBuffer.length();
+	        /*if(randomAccessFile.getFilePointer() != 0){
+	        randomAccessFile.writeBytes(System.getProperty("line.separator"));
+	        }*/
+	        randomAccessFile.writeBytes(stringBuffer.toString());
+	        randomAccessFile.writeBytes(System.getProperty("line.separator"));
+	      }
+
+	    public static String searchData() throws IOException {
+	        //Setting file pointer to start of file
+	        randomAccessFile.seek(0);
+	        System.out.println("in search data ");
+	        String skip=null;
+	        String data = randomAccessFile.readLine();
+	        while (data != null ){
+	          String[] recordToBeSearched = data.split(":");
+	          String name = recordToBeSearched[0];
+	          if(name != null && name.equals(FeedData.name)){
+	        	  skip=recordToBeSearched[1].trim();
+	        	  back = recordToBeSearched[0].length()+recordToBeSearched[1].length()+1;
+	        	  break ;
+	          }
+	          data = randomAccessFile.readLine();
+	        }
+	        System.out.println("in search data "+skip);
+	        return skip;
+	        
+	      }
 
 }
