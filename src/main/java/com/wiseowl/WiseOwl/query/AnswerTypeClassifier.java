f3b3113c2713012caea6ac08cbbad05dc6603e8c
avtar singh <asmehra95@gmail.com>
2017-04-07 19:46:30 +0530

    updated licences

--- src/main/java/com/wiseowl/WiseOwl/query/AnswerTypeClassifier.java
+++ src/main/java/com/wiseowl/WiseOwl/query/AnswerTypeClassifier.java
@@ -1,4 +1,23 @@
-
+/* Copyright 2008-2011 Grant Ingersoll, Thomas Morton and Drew Farris
+        *
+        *    Licensed under the Apache License, Version 2.0 (the "License");
+        *    you may not use this file except in compliance with the License.
+        *    You may obtain a copy of the License at
+        *
+        *        http://www.apache.org/licenses/LICENSE-2.0
+        *
+        *    Unless required by applicable law or agreed to in writing, software
+        *    distributed under the License is distributed on an "AS IS" BASIS,
+        *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        *    See the License for the specific language governing permissions and
+        *    limitations under the License.
+        * -------------------
+        * To purchase or learn more about Taming Text, by Grant Ingersoll, Thomas Morton and Drew Farris, visit
+        * http://www.manning.com/ingersoll
+        * This code has been modified and upgraded by WiseOwl Team, Avtar Singh, Sumit Kumar and Yuvraj Singh.
+        * Modifications are copyright 2016-2017 WiseOwl Team, Avtar Singh, Sumit Kumar and Yuvraj Singh
+        * https://www.linkedin.com/in/avtar-singh-6a481a124/
+        */
 package com.wiseowl.WiseOwl.query;
 
 import java.io.File;
@@ -21,28 +40,28 @@ import opennlp.tools.postag.POSTaggerME;
 
 public class AnswerTypeClassifier {
 
-  
-  private MaxentModel model;
-  private double[] probs;
-  private AnswerTypeContextGenerator atcg;
 
-  public AnswerTypeClassifier(MaxentModel model, double[] probs, AnswerTypeContextGenerator atcg) {
-    this.model = model;
-    this.probs = probs;
-    this.atcg = atcg;
-  }
+    private MaxentModel model;
+    private double[] probs;
+    private AnswerTypeContextGenerator atcg;
+
+    public AnswerTypeClassifier(MaxentModel model, double[] probs, AnswerTypeContextGenerator atcg) {
+        this.model = model;
+        this.probs = probs;
+        this.atcg = atcg;
+    }
 
 
-  //<start id="atc.compute"/>
-  public String computeAnswerType(Parse question) {
-    double[] probs = computeAnswerTypeProbs(question);//<co id="atc.getprobs"/>
-    return model.getBestOutcome(probs);//<co id="atc.outcome"/>
-  }
+    //<start id="atc.compute"/>
+    public String computeAnswerType(Parse question) {
+        double[] probs = computeAnswerTypeProbs(question);//<co id="atc.getprobs"/>
+        return model.getBestOutcome(probs);//<co id="atc.outcome"/>
+    }
 
-  public double[] computeAnswerTypeProbs(Parse question) {
-    String[] context = atcg.getContext(question);//<co id="atc.context"/>
-    return model.eval(context, probs);//<co id="atc.evaluate"/>
-  }
+    public double[] computeAnswerTypeProbs(Parse question) {
+        String[] context = atcg.getContext(question);//<co id="atc.context"/>
+        return model.eval(context, probs);//<co id="atc.evaluate"/>
+    }
   /*
   <calloutlist>
       <callout arearefs="atc.getprobs"><para>Get the probabilities of an Answer Type by calling computeAnswerTypeProbs</para></callout>
@@ -52,54 +71,56 @@ public class AnswerTypeClassifier {
 
   </calloutlist>
   */
-  //<end id="atc.compute"/>
+    //<end id="atc.compute"/>
 
-  /** Train the answer model
-   * <p>
-   *  Hint:
-   *  <pre>
-   *  mvn exec:java -Dexec.mainClass=com.tamingtext.qa.AnswerTypeClassifier \
-   *    -Dexec.args="dist/data/questions-train.txt en-answer.bin" \
-   *    -Dmodel.dir=../../opennlp-models \
-   *    -Dwordnet.dir=../../Wordnet-3.0/dict
-   *  </pre>
-   * @param args
-   * @throws IOException
-   */
-  public static void main(String[] args) throws IOException {
-	  if (args.length < 2) {
-      System.err.println("Usage: AnswerTypeClassifier <trainFile> <modelFile>");
-      System.exit(1);
-    }
-	
-    String trainFile = args[0];
-    File outFile = new File(args[1]);
-    String modelsDirProp = System.getProperty("model.dir");
-    File modelsDir = new File(modelsDirProp);
-    String wordnetDir = System.getProperty("wordnet.dir");
+    /**
+     * Train the answer model
+     * <p>
+     * Hint:
+     * <pre>
+     *  mvn exec:java -Dexec.mainClass=com.tamingtext.qa.AnswerTypeClassifier \
+     *    -Dexec.args="dist/data/questions-train.txt en-answer.bin" \
+     *    -Dmodel.dir=../../opennlp-models \
+     *    -Dwordnet.dir=../../Wordnet-3.0/dict
+     *  </pre>
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            System.err.println("Usage: AnswerTypeClassifier <trainFile> <modelFile>");
+            System.exit(1);
+        }
 
-    InputStream chunkerStream = new FileInputStream(
-        new File(modelsDir,"en-chunker.bin"));
-    ChunkerModel chunkerModel = new ChunkerModel(chunkerStream);
-    ChunkerME chunker = new ChunkerME(chunkerModel);
-    InputStream posStream = new FileInputStream(
-        new File(modelsDir,"en-pos-maxent.bin"));
-    POSModel posModel = new POSModel(posStream);
-    POSTaggerME tagger =  new POSTaggerME(posModel);
-    Parser parser = new ChunkParser(chunker, tagger);
-    AnswerTypeContextGenerator actg = new AnswerTypeContextGenerator(new File(wordnetDir));
-    //<start id="atc.train"/>
-    AnswerTypeEventStream es = new AnswerTypeEventStream(trainFile,
-            actg, parser);
-    GISModel model = GIS.trainModel(100, new TwoPassDataIndexer(es, 3));//<co id="atc.train.do"/>
-    GISModelWriter writer = new SuffixSensitiveGISModelWriter(model,outFile);
-    writer.persist();
-    //new DoccatModel("en", model).serialize(new FileOutputStream(outFile));
+        String trainFile = args[0];
+        File outFile = new File(args[1]);
+        String modelsDirProp = System.getProperty("model.dir");
+        File modelsDir = new File(modelsDirProp);
+        String wordnetDir = System.getProperty("wordnet.dir");
+
+        InputStream chunkerStream = new FileInputStream(
+                new File(modelsDir, "en-chunker.bin"));
+        ChunkerModel chunkerModel = new ChunkerModel(chunkerStream);
+        ChunkerME chunker = new ChunkerME(chunkerModel);
+        InputStream posStream = new FileInputStream(
+                new File(modelsDir, "en-pos-maxent.bin"));
+        POSModel posModel = new POSModel(posStream);
+        POSTaggerME tagger = new POSTaggerME(posModel);
+        Parser parser = new ChunkParser(chunker, tagger);
+        AnswerTypeContextGenerator actg = new AnswerTypeContextGenerator(new File(wordnetDir));
+        //<start id="atc.train"/>
+        AnswerTypeEventStream es = new AnswerTypeEventStream(trainFile,
+                actg, parser);
+        GISModel model = GIS.trainModel(100, new TwoPassDataIndexer(es, 3));//<co id="atc.train.do"/>
+        GISModelWriter writer = new SuffixSensitiveGISModelWriter(model, outFile);
+        writer.persist();
+        //new DoccatModel("en", model).serialize(new FileOutputStream(outFile));
     /*
     <calloutlist>
         <callout arearefs="atc.train.do"><para>Using the event stream, which feeds us training examples, do the actual training using OpenNLP's Maxent classifier.</para></callout>
     </calloutlist>
     */
-    //<end id="atc.train"/>
-  }
+        //<end id="atc.train"/>
+    }
 }
